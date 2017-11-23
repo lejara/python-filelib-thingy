3d9209c1cb1a702d2668ff6c1b83ea6a5255f265
Jay Yin <jayyin11043@hotmail.com>
2017-11-23 10:15:37 -0500

    trying someything different for pytest config

--- python/File_Manip.py
+++ python/File_Manip.py
@@ -3,3 +3,6 @@ import os
 def getFileName(pathName):
     """ returns filename when path is given regardless of slash types or OS file directory type"""
     return os.path.basename(os.path.normpath(pathName))
+
+def test_getFileName():
+    assert getFileName("gg/hello\something/file.exe") == "file.exe"

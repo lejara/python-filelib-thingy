3a33fb215efa1895435281a6c8a4757ba3d0a3d3
Jay Yin <jayyin11043@hotmail.com>
2017-11-23 11:12:57 -0500

    milestone

--- python/File_Manipy.py
+++ python/File_Manipy.py
@@ -5,3 +5,5 @@ def getFileName(pathName):
     """ returns filename when path is given regardless of slash types or OS file directory type"""
     return os.path.basename(os.path.normpath(pathName))
 
+
+
--- python/test_tests.py
+++ python/test_tests.py
@@ -2,3 +2,4 @@ import File_Manipy
 
 def test_getFileName():
     assert File_Manipy.getFileName("gg/hello\something/file.exe") == "file.exe"
+    

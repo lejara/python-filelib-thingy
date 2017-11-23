1788ae361408061cbc5b63f4338a57fccb5ba802
Jay Yin <jayyin11043@hotmail.com>
2017-11-23 11:35:47 -0500

    added file size and testing

--- python/test_tests.py
+++ python/test_tests.py
@@ -3,3 +3,5 @@ import File_Manipy
 def test_getFileName():
     assert File_Manipy.getFileName("gg/hello\something/file.exe") == "file.exe"
     
+def test_getFileSize1():
+    assert File_Manipy.getFileSize("test_file.txt") == 43
